7efd52088fc1a1e6445824f9eddde2f90e18713b
Krzysztof Dudzik (PGS Software) <kdudzik@pgs-soft.com>
2017-03-21 16:24:41 +0100

    clean asp temporary

---
+++ cleanAspTemporary.bat
@@ -0,0 +1,5 @@
+del "%WINDIR%\Microsoft.NET\Framework64\v2.0.50727\Temporary ASP.NET Files\*" /Q /S
+del "%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\Temporary ASP.NET Files\*" /Q /S
+del "%WINDIR%\Microsoft.NET\Framework\v2.0.50727\Temporary ASP.NET Files\*" /Q /S
+del "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\Temporary ASP.NET Files\*" /Q /S
+del "%TEMP%\Temporary ASP.NET Files\*" /Q /S
